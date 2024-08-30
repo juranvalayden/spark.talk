3d011f877cdb0c7ff8e84add7225e6d226cac024
Juran Valayden <JuranV@rcsgroup.co.za>
2024-08-30 10:51:07 +0200

    added a single property to customer

--- Spark.Talk.Sept.A.SRP/Solid.Principles/DataStores/CustomerDataStore.cs
+++ Spark.Talk.Sept.A.SRP/Solid.Principles/DataStores/CustomerDataStore.cs
@@ -1,8 +1,13 @@
 ﻿using Solid.Principles.Models;
 
 namespace Solid.Principles.DataStores;
-
 public class CustomerDataStore
 {
     public List<Customer> Customers { get; set; } = [];
+
+    public CustomerDataStore()
+    {
+        for (var i = 0; i < 10; i++)
+            Customers.Add(new Customer());
+    }
 }
--- Spark.Talk.Sept.A.SRP/Solid.Principles/Models/Customer.cs
+++ Spark.Talk.Sept.A.SRP/Solid.Principles/Models/Customer.cs
@@ -1,4 +1,7 @@
 ﻿namespace Solid.Principles.Models;
 public class Customer
 {
+    public Guid Id { get; set; }
+
+    public Customer() => Id = Guid.NewGuid();
 }
